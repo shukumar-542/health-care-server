593c3ae267ea6fddad8ff4d348a5cd6afe4589b9
shukumar-542 <shukumar542@gmail.com>
2024-03-16 16:00:41 +0600

    check

--- src/app.ts
+++ src/app.ts
@@ -41,7 +41,7 @@ app.use(globalErrorHandler);
 app.use((req: Request, res: Response, next: NextFunction) => {
   res.status(httpStatus.NOT_FOUND).json({
     success: false,
-    message: 'Not Found',
+    message: 'Not Founds',
     errorMessages: [
       {
         path: req.originalUrl,
